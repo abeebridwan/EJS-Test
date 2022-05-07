708bd74275f4b386893687663f57e1a45a5c464a
Abeeb Ridwan <igortunde@yahoo.com>
2022-05-08 00:50:25 +0100

    feat: view engine setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+//ejs view engine setup
+app.set('views', './views');
+app.set('view engine', 'ejs')
+
+app.get('/', (_, res) => {
+  res.render('index')
+})
+
+
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`> Ready on http://localhost:${port}`)
+})
