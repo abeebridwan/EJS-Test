b8e3aa6e1c4cf24069abbc82f5520cbfb29ea554
Abeeb Ridwan <igortunde@yahoo.com>
2022-05-08 01:20:20 +0100

    style: style added

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const port = process.env.PORT || 3000;
 app.set('views', './views');
 app.set('view engine', 'ejs')
 
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (_, res) => {
   res.render('index')
 })
-
 
 app.listen(port, (err) => {
   if (err) throw err;
